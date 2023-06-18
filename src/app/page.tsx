7fd5e43396d910c9649d7466348178fb51f0d3ae
Emil Jansson <ejansson.swe@gmail.com>
2023-06-18 22:26:05 +0200

    🚧 Work on fetching some data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import SectionTitle from '@/components/SectionTitle';
+'use-client';
+import Image from 'next/image';
 
-export default function Home() {
+import SectionTitle from '@/components/SectionTitle';
+import { Movie } from '@/types/Movies';
+import { getMovieDetails, getTrendingMovies } from '@/utils/movies';
+
+export default async function Home() {
+  const tredningMovies = await getTrendingMovies();
+  const leftTrending = await getMovieDetails(tredningMovies.results[0]);
+  const rightTrending = await getMovieDetails(tredningMovies.results[1]);
+
   return (
-    <>
+    <div className="">
       <SectionTitle>Home</SectionTitle>
-    </>
+
+      <h3>Trending</h3>
+
+      <div className="grid grid-cols-2">
+        <div key={leftTrending.id}>
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${leftTrending.poster_path}`}
+            alt={`Poster of ${leftTrending.title}`}
+            width="500"
+            height="700"
+          />
+          <h4>{leftTrending.title}</h4>
+        </div>
+      </div>
+    </div>
   );
 }
