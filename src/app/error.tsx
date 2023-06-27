ed543e42cf3de20b71ac8143dd28b2eaace5e35d
Emil Jansson <ejansson.swe@gmail.com>
2023-06-27 18:17:07 +0200

    💄 Style error page and spinner

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,9 +7,9 @@ type ErrorPageProps = {
 export default function Error({ reset }: ErrorPageProps) {
   return (
     <div>
-      <h2 className="text-lg font-bold">Something went wrong!</h2>
+      <h2 className="mt-4 text-2xl font-bold">Something went wrong!</h2>
       <button
-        className="rounded-sm bg-zinc-300 px-3 py-2 text-black"
+        className="mt-4 rounded-full border border-neutral-50 px-4 py-1 text-neutral-50 hover:bg-neutral-50 hover:text-black"
         onClick={() => reset()}
       >
         Try again
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,9 @@
 import Spinner from '@/components/Spinner';
 
 export default function LoadingPage() {
-  return <Spinner />;
+  return (
+    <div className="flex justify-center py-8">
+      <Spinner />
+    </div>
+  );
 }
