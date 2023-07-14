69f853d07506ae6000fd9c553672681b6b400f36
Emil Jansson <ejansson.swe@gmail.com>
2023-07-14 14:11:10 +0200

    💄 Set max width for navigation

--- src/components/NavigationAside.tsx
+++ src/components/NavigationAside.tsx
@@ -31,7 +31,7 @@ export default function NavigationAside() {
       className={cn([
         'max-w-screen-xs fixed top-0 z-30 col-span-2 row-span-full flex h-full w-full flex-col',
         'items-center border-r border-zinc-600 bg-neutral-900 p-3 shadow-2xl transition-all duration-200',
-        'w-[calc(100vw-4rem)] min-[320px]:w-[calc(100vw-7rem)] desktop:static desktop:left-auto desktop:w-auto',
+        'w-[calc(100vw-4rem)] max-w-screen-sm min-[320px]:w-[calc(100vw-8rem)] desktop:static desktop:left-auto desktop:w-auto',
         { 'translate-x-[-100vw] desktop:translate-x-0': !navOpen },
         { 'translate-x-0': navOpen },
       ])}
@@ -44,9 +44,9 @@ export default function NavigationAside() {
 
       <nav className="grid h-full flex-1 place-items-center bg-neutral-900">
         <ul className="grid grid-cols-1 gap-8">
-          {links.map((link) => {
-            return <NavigationLink key={link.href} link={link} />;
-          })}
+          {links.map((link) => (
+            <NavigationLink key={link.href} link={link} />
+          ))}
         </ul>
       </nav>
     </aside>
