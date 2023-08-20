d20b5f28796461e3b52a2d06e675e3072ff56e20
Emil Jansson <e.jansson@icloud.com>
2023-08-20 18:38:54 +0200

    🏷️ Add type for movie details

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,3 +1,5 @@
+import { Genre } from './Genre';
+
 export type Movie = {
   adult: boolean;
   backdrop_path: string;
@@ -11,6 +13,35 @@ export type Movie = {
   genre_ids: number[];
   popularity: number;
   release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type MovieDetails = {
+  adult: boolean;
+  backdrop_path: string;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  title: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
