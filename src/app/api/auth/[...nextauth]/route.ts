1423a00871aff34fa600bd46d13662f5a9ee9307
Emil Jansson <ejansson.swe@gmail.com>
2023-10-01 17:19:40 +0200

    ✨ Implement Prisma adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
+import { env } from 'process';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
-import { env } from 'process';
+import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+const prisma = new PrismaClient();
 
 const clientId = env.GITHUB_ID ?? '';
 const clientSecret = env.GITHUB_SECRET ?? '';
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId,
