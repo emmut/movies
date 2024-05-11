34681ba1a848590f956a99e163b3254cb50e616b
Emil Jansson <emil.jansson@compileit.com>
2024-05-11 11:04:40 +0200

    📈 add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import LayoutClient from '@/components/LayoutClient';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'], fallback: ['sans-serif'] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <LayoutClient>{children}</LayoutClient>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
