116b5683b2846c5446a13d7b890d45fcc3b07f27
Emil Jansson <emil.jansson@compileit.com>
2025-04-05 16:07:12 +0200

    🔧 disable is pseudo class in postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-preset-env': {},
+    'postcss-preset-env': {
+      features: {
+        'is-pseudo-class': false, // Disable the transformation of :is()
+      },
+    },
   },
 };
