bf1a923b34d79fada91d00ee3b5db06a6b33baae
Emil Jansson <emil.jansson@compileit.com>
2025-04-05 15:56:45 +0200

    🔧 remove postcss presetenv

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-preset-env': {
-      features: {
-        'is-pseudo-class': false // Disable the transformation of :is()
-      }
-    },
   },
 };
