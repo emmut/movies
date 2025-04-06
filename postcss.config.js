b490f8ead1004bb67f4dbb1ea6fdb22b7feabd69
Emil Jansson <emil.jansson@compileit.com>
2025-04-06 13:23:26 +0200

    🔥 remove comment

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '@tailwindcss/postcss': {},
     'postcss-preset-env': {
       features: {
-        'is-pseudo-class': false, // Disable the transformation of :is()
+        'is-pseudo-class': false,
       },
     },
   },
