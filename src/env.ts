80fcf19c2559fc5f5f2daa181c85915f99967b9b
Emil Jansson <ejansson.swe@gmail.com>
2025-06-06 18:45:24 +0200

    🔥 remove unused varibale

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,6 @@ export const env = createEnv({
   server: {
     MOVIE_DB_ACCESS_TOKEN: z.string().min(1),
     BETTER_AUTH_SECRET: z.string().min(1),
-    BETTER_AUTH_URL: z.url(),
     DATABASE_URL: z.url(),
     DISCORD_CLIENT_ID: z.string().min(1),
     DISCORD_CLIENT_SECRET: z.string().min(1),
@@ -19,7 +18,6 @@ export const env = createEnv({
   runtimeEnv: {
     MOVIE_DB_ACCESS_TOKEN: process.env.MOVIE_DB_ACCESS_TOKEN,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
