fe429526f8ef3635250e78900f6f31dd466044e3
Emil Jansson <ejansson.swe@gmail.com>
2025-06-07 19:39:57 +0200

    💄 add attribution via logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
+
 function Footer() {
   return (
-    <footer className="mt-auto py-8">
+    <footer className="mt-auto flex flex-col items-center gap-4 py-16">
       <p className="text-muted-foreground/80 text-center text-xs italic">
         This product uses the TMDb API but is not endorsed or certified by TMDb
       </p>
+
+      <a
+        href="https://www.themoviedb.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/tmdb-logo.svg"
+          alt="TMDb Logo"
+          width={150}
+          height={150}
+          className="mx-auto"
+          loading="eager"
+        />
+      </a>
     </footer>
   );
 }
