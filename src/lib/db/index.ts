219518b9b20a8942c1ab0f53f8336a2d0be5939e
Emil Jansson <ejansson.swe@gmail.com>
2025-07-26 14:43:02 +0200

    ♻️ simplify db setup and folder structure

--- src/lib/db/index.ts
+++
@@ -1,9 +0,0 @@
-import * as authSchema from '@/db/schema/auth';
-import * as listsSchema from '@/db/schema/lists';
-import { env } from '@/env';
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-
-const schema = { ...authSchema, ...listsSchema };
-const sql = neon(env.DATABASE_URL);
-export const db = drizzle({ client: sql, schema });
