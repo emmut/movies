a5d06c8a4408b3b9315cc64df5a88df624a46766
Emil Jansson <ejansson.swe@gmail.com>
2025-09-06 19:00:05 +0200

    🏷️ type external ids reponse

--- src/types/tv-show.ts
+++ src/types/tv-show.ts
@@ -111,5 +111,14 @@ export type TvSimilar = {
 };
 
 export type TmdbExternalIdsResponse = {
-  imdb_id: string;
+  id: number;
+  imdb_id: string | null;
+  freebase_mid: string | null;
+  freebase_id: string | null;
+  tvdb_id: number | null;
+  tvrage_id: number | null;
+  wikidata_id: string | null;
+  facebook_id: string | null;
+  instagram_id: string | null;
+  twitter_id: string | null;
 };
