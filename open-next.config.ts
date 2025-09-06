e8f7109f568274ed0ab66dba25c5f5ddd1ded62e
Emil Jansson <ejansson.swe@gmail.com>
2025-09-06 12:59:40 +0200

    🔧 fix shared tag cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,14 +1,14 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
-import d1NextTagCache from '@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache';
+import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache';
 //import { withFilter, softTagFilter } from "@opennextjs/cloudflare/overrides/tag-cache/tag-cache-filter";
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
   queue: doQueue,
   // This is only required if you use On-demand revalidation
-  tagCache: d1NextTagCache,
+  tagCache: doShardedTagCache({ baseShardSize: 12, regionalCache: true }),
   //If you don't use `revalidatePath`, you can also filter internal soft tags using the `softTagFilter`
   // tagCache: withFilter({
   //   tagCache: d1NextTagCache,
