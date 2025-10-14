49a0db8cd6a799157da74c612bd50c69fea3edc2
Emil Jansson <emil.jansson@compileit.com>
2025-10-14 22:03:48 +0200

    🔧 remove poscss preset env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,5 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-preset-env': {
-      features: {
-        'is-pseudo-class': false,
-        'custom-properties': false,
-      },
-    },
   },
 };
