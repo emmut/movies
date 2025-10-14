7bb1efb9d023f75be6ccfb495958694b837111d0
Emil Jansson <emil.jansson@compileit.com>
2025-10-14 17:42:11 +0200

    ♻️ move suspense

--- src/components/app-sidebar-wrapper.tsx
+++ src/components/app-sidebar-wrapper.tsx
@@ -5,17 +5,19 @@ import { UserNav } from './app-sidebar-user-nav';
 
 export async function AppSidebarWrapper() {
   return (
-    <AppSidebar
-      userNav={
-        <Suspense fallback={<UserNav.Ghost />}>
-          <UserNav />
-        </Suspense>
-      }
-      userFooter={
-        <Suspense fallback={<UserFooter.Ghost />}>
-          <UserFooter />
-        </Suspense>
-      }
-    />
+    <Suspense>
+      <AppSidebar
+        userNav={
+          <Suspense fallback={<UserNav.Ghost />}>
+            <UserNav />
+          </Suspense>
+        }
+        userFooter={
+          <Suspense fallback={<UserFooter.Ghost />}>
+            <UserFooter />
+          </Suspense>
+        }
+      />
+    </Suspense>
   );
 }
